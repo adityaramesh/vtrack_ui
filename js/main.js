60ff52260f22acf53eac9100e4beb10d3809f938
Aditya Ramesh <_@adityaramesh.com>
2013-08-22 02:35:41 -0700

    Optimized for Apple devices.

--- js/main.js
+++ js/main.js
@@ -11,28 +11,36 @@ $(function() {
 
 	$("#go").click(function() {
 		// var v = $("#version :selected").text();
-		var c = $("#camera :selected").text();
 		// var t = $("#time :selected").text();
-		var r = window.devicePixelRatio;
-
+		var c = $("#camera :selected").text();
 		var path = "";
-		var w = 0;
-		var h = 0;
+		var vw = 0;
+		var vh = 0;
+		var ew = 0;
+		var eh = 0;
 
 		switch (c) {
-		case "19": path = "data/cam019.1.avi"; w = 704 / r; h = 480 / r; break;
-		case "36": path = "data/cam036.1.avi"; w = 640 / r; h = 480 / r; break;
-		case "57": path = "data/cam057.1.avi"; w = 704 / r; h = 480 / r; break;
-		case "114": path = "data/cam114.1.avi"; w = 704 / r; h = 480 / r; break;
+		case "19": path = "data/cam019.1.avi"; vw = 704; vh = 480; break;
+		case "36": path = "data/cam036.1.avi"; vw = 640; vh = 480; break;
+		case "57": path = "data/cam057.1.avi"; vw = 704; vh = 480; break;
+		case "114": path = "data/cam114.1.avi"; vw = 704; vh = 480; break;
 		}
 
+		var r = 1 / window.devicePixelRatio;
+		if (vw * r > $(window).width()) {
+			r = $(window).width() / vw;
+		}
+
+		ew = Math.floor(vw * r);
+		eh = Math.floor(vh * r);
+
 		$("#player").prepend(" \
-			<video width='" + w + "' height='" + h + "' id='video' controls> \
+			<video width='" + ew + "' height='" + eh + "' id='video' controls> \
 				<source src='" + path + "' type='video/avi'> \
 			</video> \
 		");
-		$("#selection").animate({left: -800});
-		$("#player").animate({left: -800});
+		$("#selection").animate({left: -$("#selection").width()});
+		$("#player").animate({left: -$("#player").width()});
 	});
 	$("#back").click(function() {
 		$("#selection").animate({left: 0});
